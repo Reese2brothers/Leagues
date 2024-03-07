338bf2ddf46296501ee01e51598cb534b10f7d69
Reese <reese2brothers@gmail.com>
2024-03-07 11:20:47 +0200

    init

---
+++ app/src/main/java/com/tirokes/leagues/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tirokes.leagues
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
